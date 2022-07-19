d674b1d3c1924303e01699a30d5aae775d73c8eb
Pavel Rybakov <me@pavelrybakov.ru>
2022-07-19 18:54:25 +0300

    Buy, sell, take

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -11,64 +11,70 @@ public class CoffeeMachine {
     static int money = 550;
 
     public static void main(String[] args) {
-        System.out.println("Write how many ml of water the coffee machine has:");
-        water = scanner.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        milk = scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        beans = scanner.nextInt();
-        System.out.println("Write how many cups of coffee you will need:");
-        int cups = scanner.nextInt();
-        int calculatedCups = calculateCoffee();
-        if (cups == calculatedCups) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (cups < calculatedCups) {
-            System.out.println("Yes, I can make that amount of coffee (and even " + (calculatedCups - cups) + " more than that");
-        } else {
-            System.out.println("No, I can make only " + calculatedCups + " cup(s) of coffee");
-        }
 
+        printState();
+        System.out.println("Write action (buy, fill, take):");
+        String action = scanner.next();
+        if (action.equals("buy")) {
+            buyCoffee();
+        }
+        if (action.equals("fill")) {
+            fillCoffee();
+        }
+        if (action.equals("take")) {
+            takeMoney();
+        }
+        printState();
+
+    }
+
+    private static void takeMoney() {
+        System.out.println("I gave you $" + money);
+        money = 0;
+    }
+
+    private static void fillCoffee() {
+        System.out.println("Write how many ml of water you want to add:");
+        water = water + scanner.nextInt();
+        System.out.println("Write how many ml of milk you want to add: ");
+        milk = milk + scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        beans = beans + scanner.nextInt();
+        System.out.println("Write how many disposable cups of coffee you want to add:");
+        cups = cups + scanner.nextInt();
+    }
+
+    private static void buyCoffee() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        int sortOfCoffee = scanner.nextInt();
+        if (sortOfCoffee == 1) {
+            water -= 250;
+            beans -= 16;
+            cups -=1;
+            money += 4;
+        }
+        if (sortOfCoffee == 2) {
+            water -= 350;
+            milk -= 75;
+            beans -= 20;
+            cups -=1;
+            money += 7;
+        }
+        if (sortOfCoffee == 3) {
+            water -= 200;
+            milk -= 100;
+            beans -= 12;
+            cups -=1;
+            money += 6;
+        }
     }
 
     public static void printState() {
         System.out.println("The coffee machine has:");
         System.out.println(water + " ml of water");
-        System.out.println(milk +" ml of milk");
+        System.out.println(milk + " ml of milk");
         System.out.println(beans + " of coffee beans");
         System.out.println(cups + " disposable cups");
         System.out.println("$" + money + " of money");
-    }
-
-    public static void printCoffee() {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
-    }
-
-    public static int calculateCoffee() {
-        final int WATER = 200;
-        final int MILK = 50;
-        final int BEANS = 15;
-
-        int minCupsWater = water / WATER;
-        int minCupsMilk = milk / MILK;
-        int minCupsBeans = beans / BEANS;
-
-        if (minCupsWater <= minCupsBeans && minCupsWater <= minCupsMilk) {
-            return minCupsWater;
-        }
-
-        if (minCupsMilk <= minCupsWater && minCupsMilk <= minCupsBeans) {
-            return minCupsMilk;
-        }
-
-        if (minCupsBeans <= minCupsWater && minCupsBeans <= minCupsMilk) {
-            return minCupsBeans;
-        }
-        return 0;
     }
 }
